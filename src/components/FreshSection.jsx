6910c50728d2e0e05cb8f164f0294d837aa1c5aa
Raunak Scarlet <raunakscarlet@gmail.com>
2024-01-12 04:00:21 +0530

    made FreshSection responsive

--- src/components/FreshSection.jsx
+++ src/components/FreshSection.jsx
@@ -5,15 +5,15 @@ import cabbage from "../assets/product13-free-img.jpg";
 
 const FreshSection = () => {
     return (
-        <div className="w-full  relative bg-[#F8F6F2] flex flex-col items-center mt-20 ">
+        <div className="w-full relative bg-[#F8F6F2] flex flex-col items-center mt-20  ">
             <img
                 src={basilLeaf}
                 alt="basilLeaf"
                 className="absolute -top-10  w-[12rem]"
             />
-            <div className="py-28 flex gap-8">
-                <div className="bg-white w-[23rem] h-[23rem] rounded-md shadow-md  flex flex-col relative overflow-hidden py-8 px-10">
-                    <div className="relative z-10">
+            <div className="py-28 grid  grid-cols-1 md:grid-cols-3 items-center  gap-8 md:gap-8 mx-4">
+                <div className="bg-white w-auto    h-[23rem] rounded-md shadow-md  flex flex-col  overflow-hidden border-2 relative ">
+                    <div className="p-4 relative z-10  ">
                         <h3 className="font-bold text-3xl  py-2 ">
                             Farm Fresh Fruits
                         </h3>
@@ -26,15 +26,14 @@ const FreshSection = () => {
                             <i className="fa-solid fa-arrow-right ml-4"></i>
                         </button>
                     </div>
-
                     <img
                         src={lemon}
                         alt="lemon"
-                        className=" absolute w-72 right-4 top-0   "
+                        className=" absolute w-72 right-4 top-2 md:top-20 lg:top-0 "
                     />
                 </div>
-                <div className="bg-white w-[23rem] h-[23rem] rounded-md shadow-md  flex flex-col relative overflow-hidden py-8 px-10">
-                    <div className="relative z-10">
+                <div className="bg-white w-auto  h-[23rem] rounded-md shadow-md  flex flex-col  overflow-hidden  border-2 relative ">
+                    <div className="p-4 relative z-10  ">
                         <h3 className="font-bold text-3xl  py-2 ">
                             Fresh Vegetables
                         </h3>
@@ -47,15 +46,15 @@ const FreshSection = () => {
                             <i className="fa-solid fa-arrow-right ml-4"></i>
                         </button>
                     </div>
-
                     <img
                         src={cabbage}
                         alt="cabbage"
-                        className=" absolute w-72 right-4 top-0   "
+                        className=" absolute w-72 right-4 top-2 md:top-20 lg:top-0 "
                     />
                 </div>
-                <div className="bg-white w-[23rem] h-[23rem] rounded-md shadow-md  flex flex-col relative overflow-hidden py-8 px-10">
-                    <div className="relative z-10">
+
+                <div className="bg-white w-auto  h-[23rem] rounded-md shadow-md  flex flex-col  overflow-hidden  border-2 relative  ">
+                    <div className="p-4 relative z-10  ">
                         <h3 className="font-bold text-3xl  py-2 ">
                             Organic Legume
                         </h3>
@@ -68,25 +67,26 @@ const FreshSection = () => {
                             <i className="fa-solid fa-arrow-right ml-4"></i>
                         </button>
                     </div>
-
                     <img
                         src={pulse}
                         alt="pulse"
-                        className=" absolute w-72 right-4 top-0   "
+                        className=" absolute w-72 right-4 top-2 md:top-20 lg:top-0 "
                     />
                 </div>
+
+               
             </div>
-            <div className="w-full h-44 px-32 bg-black flex justify-between items-center ">
-                <h2 className="text-4xl text-white">
+            <div className="w-full min-h-32  px-4 sm:px-10 md:px-16  bg-black flex justify-between items-center ">
+                <h2 className="md:text-2xl lg:text-4xl text-white">
                     Get 25% Off On Your First Purchase!
                 </h2>
-                <button className="bg-[#60a610] w-40 h-12  text-white rounded">
-                    <span className="">SHOP NOW</span>
-                    <i className="fa-solid fa-arrow-right ml-4"></i>
+                <button className="bg-[#60a610] w-40 h-8 sm:h-12  text-white rounded">
+                    <span className="text-xs sm:text-xl">SHOP NOW</span>
+                    <i className="fa-solid fa-arrow-right ml-4 text-xs sm:text-xl"></i>
                 </button>
             </div>
-            <div className="w-full h-24 flex items-center justify-center bg-white" >
-                <h1 className="text-3xl font-bold text-black">
+            <div className="w-full h-24 flex items-center justify-center bg-white">
+                <h1 className="text-md sm:text-lg md:text-2xl lg:text-3xl  font-bold text-black">
                     Try It For Free. No Registration Needed.
                 </h1>
             </div>
